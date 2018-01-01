2055425af374cda807ad3244c681a49bfd6fa3f6
sujaykondanekar <sujaykondanekar@gmail.com>
2018-01-01 16:16:46 +0530

    11

--- WPFApp1/MainWindow.xaml.cs
+++ WPFApp1/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WPFApp1
         public MainWindow()
         {
             InitializeComponent();
-            lblDisplay.Content = "My demo app version 10";
+            lblDisplay.Content = "My demo app version 11";
         }
     }
 }
