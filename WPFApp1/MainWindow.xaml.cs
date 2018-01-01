027478b97dba50a264e1ca13a674e598b2bff977
sujaykondanekar <sujaykondanekar@gmail.com>
2018-01-01 16:51:09 +0530

    My code changes

--- WPFApp1/MainWindow.xaml.cs
+++ WPFApp1/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WPFApp1
         public MainWindow()
         {
             InitializeComponent();
-            lblDisplay.Content = "My demo app version 16";
+            lblDisplay.Content = "My demo app version 17";
         }
     }
 }
