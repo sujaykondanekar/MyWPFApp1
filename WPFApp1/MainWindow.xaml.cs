bf9984d8b971ef5ae5a609c2c55b469ac36a1f93
sujaykondanekar <sujaykondanekar@gmail.com>
2018-01-01 15:39:43 +0530

    commiting the changes to server

--- WPFApp1/MainWindow.xaml.cs
+++ WPFApp1/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WPFApp1
         public MainWindow()
         {
             InitializeComponent();
-            lblDisplay.Content = "My demo app version 7";
+            lblDisplay.Content = "My demo app version 8";
         }
     }
 }
