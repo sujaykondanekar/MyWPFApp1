bcd86f7cb401c96198168a0e975d173f758a4ca6
anirudhchandrashekarrao <anirudhchandrashekarrao@gmail.com>
2018-04-28 11:53:08 +0530

    Update MainWindow.xaml.cs

--- WPFApp1/MainWindow.xaml.cs
+++ WPFApp1/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WPFApp1
         public MainWindow()
         {
             InitializeComponent();
-            lblDisplay.Content = "Review 4";
+            lblDisplay.Content = "Review 5";
         }
     }
 }
